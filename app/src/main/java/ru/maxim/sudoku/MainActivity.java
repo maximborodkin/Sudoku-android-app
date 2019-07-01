2db86784f22b4da63029790f907944faba532094
maximborodkin <maxim.borodkin1999@yandex.ru>
2019-07-02 00:17:48 +0300

    second commit

--- app/src/main/java/ru/maxim/sudoku/MainActivity.java
+++ app/src/main/java/ru/maxim/sudoku/MainActivity.java
@@ -8,7 +8,6 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
